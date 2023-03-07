17a2f0c602695555d94ebc1b4c0e265a7d39afe1
Balázs Sáros <balazs.saros@inspyre.it>
2023-03-07 15:09:35 +0100

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const ticketCounter = screen.getByText(/Lottery tickets: \d*/);
+  const yearCounter = screen.getByText(/Years spent: \d*/);
+  const twomatch = screen.getByText(/2 matches: \d*/);
+  const threematch = screen.getByText(/3 matches: \d*/);
+  const fourmatch = screen.getByText(/4 matches: \d*/);
+  const fivematch = screen.getByText(/5 matches: \d*/);
+  expect(ticketCounter).toBeInTheDocument();
+  expect(yearCounter).toBeInTheDocument();
+  expect(twomatch).toBeInTheDocument();
+  expect(threematch).toBeInTheDocument();
+  expect(fourmatch).toBeInTheDocument();
+  expect(fivematch).toBeInTheDocument();
+});
+
+test('renders start btn first', () => {
+  render(<App />);
+  const startBtn = screen.getByText(/Start/);
+  const pauseBtn = screen.queryByText(/Pause/);
+  expect(startBtn).toBeInTheDocument();
+  expect(pauseBtn).not.toBeInTheDocument();
+});
+
+test('renders pause and hides start btn after sim starts', () => {
+  render(<App />);
+  const startBtn = screen.getByText(/Start/);
+  fireEvent.click(startBtn);
+  const pauseBtn = screen.getByText(/Pause/);
+  const startBtnQuery = screen.queryByText(/Start/);
+  expect(pauseBtn).toBeInTheDocument();
+  expect(startBtnQuery).not.toBeInTheDocument();
 });
